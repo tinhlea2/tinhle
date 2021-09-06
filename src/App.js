271437a0d746f0d6411ea33c4aab742d4a43989d
tinhle <lethitinh1112@gmail.com>
2021-09-06 21:48:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        tinhle
+        chan ghe
         </p>
       </header>
     </div>
